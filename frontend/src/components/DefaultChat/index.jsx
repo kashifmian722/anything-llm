d88d526efafcf086d2404daa127aeab90dc60d85
kashifmian722 <64021544+kashifmian722@users.noreply.github.com>
2024-06-17 04:26:39 +0500

    Update index.jsx

--- frontend/src/components/DefaultChat/index.jsx
+++ frontend/src/components/DefaultChat/index.jsx
@@ -51,11 +51,9 @@ export default function DefaultChatContainer() {
             <span
               className={`whitespace-pre-line text-white font-normal text-sm md:text-sm flex flex-col gap-y-1 mt-2`}
             >
-              Welcome to AnythingLLM, AnythingLLM is an open-source AI tool by
-              Mintplex Labs that turns anything into a trained chatbot you can
-              query and chat with. AnythingLLM is a BYOK (bring-your-own-keys)
-              software so there is no subscription, fee, or charges for this
-              software outside of the services you want to use with it.
+              Welcome to Fabriiq Ai, Fabriiq Ai is an AI tool by
+              tekmindlabs that turns anything into a trained chatbot you can
+              query and chat with.
             </span>
           </div>
         </div>
@@ -75,9 +73,7 @@ export default function DefaultChatContainer() {
             <span
               className={`whitespace-pre-line text-white font-normal text-sm md:text-sm flex flex-col gap-y-1 mt-2`}
             >
-              AnythingLLM is the easiest way to put powerful AI products like
-              OpenAi, GPT-4, LangChain, PineconeDB, ChromaDB, and other services
-              together in a neat package with no fuss to increase your
+              |Fabriiq Ai is the easiest way to put powerful AI on wotk with no fuss to increase your
               productivity by 100x.
             </span>
           </div>
@@ -98,12 +94,10 @@ export default function DefaultChatContainer() {
               <span
                 className={`whitespace-pre-line text-white font-normal text-sm md:text-sm flex flex-col gap-y-1 mt-2`}
               >
-                AnythingLLM can run totally locally on your machine with little
-                overhead you wont even notice it's there! No GPU needed. Cloud
-                and on-premises installation is available as well.
+                Cloud and on-premises installation is available as well.
                 <br />
                 The AI tooling ecosystem gets more powerful everyday.
-                AnythingLLM makes it easy to use.
+                Fabriiq Ai makes it easy to use.
               </span>
               <a
                 href={paths.github()}
@@ -216,10 +210,10 @@ export default function DefaultChatContainer() {
             <span
               className={`whitespace-pre-line text-white font-normal text-sm md:text-sm flex flex-col gap-y-1 mt-2`}
             >
-              AnythingLLM is more than a smarter Dropbox.
+              Fabriiq Ai is more than a smarter Dropbox.
               <br />
               <br />
-              AnythingLLM offers two ways of talking with your data:
+              Fabriiq Ai offers two ways of talking with your data:
               <br />
               <br />
               <i>Query:</i> Your chats will return data or inferences found with
