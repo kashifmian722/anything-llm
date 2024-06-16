10b8d3cb2e6e3cf3d5fe72bdfe565e097a583a49
kashifmian722 <64021544+kashifmian722@users.noreply.github.com>
2024-06-17 04:31:19 +0500

    Update index.jsx

--- frontend/src/components/DefaultChat/index.jsx
+++ frontend/src/components/DefaultChat/index.jsx
@@ -99,14 +99,7 @@ export default function DefaultChatContainer() {
                 The AI tooling ecosystem gets more powerful everyday.
                 Fabriiq Ai makes it easy to use.
               </span>
-              <a
-                href={paths.github()}
-                target="_blank"
-                className="mt-5 w-fit transition-all duration-300 border border-slate-200 px-4 py-2 rounded-lg text-white text-sm items-center flex gap-x-2 hover:bg-slate-200 hover:text-slate-800 focus:ring-gray-800"
-              >
-                <GitMerge className="h-4 w-4" />
-                <p>Create an issue on Github</p>
-              </a>
+             
             </div>
           </div>
         </div>
